69ea182c56dfcb25881290a350967bf9cc6436c2
JH5256 <skdltmvhs2@naver.com>
2024-03-12 14:12:27 +0900

    MyBatis practice

---
+++ mybatis_practice-master/src/main/java/com/management/category/model/dao/CategoryDAO.java
@@ -0,0 +1,20 @@
+package com.management.category.model.dao;
+
+import com.management.category.model.dto.CategoryDTO;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CategoryDAO {
+    List<CategoryDTO> selectCategoryList(Map<String, String> parameter);
+
+    int insertCategory(CategoryDTO category);
+
+    int updateCategory(CategoryDTO category);
+
+    int deleteCategory(Map<String, String> parameter);
+
+    // * 주석을 지우고 Mapper interface(= DAO class) 역할을 하도록 내용을 작성하세요.
+
+
+}
